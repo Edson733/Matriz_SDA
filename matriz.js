fd7395d2ee56b8443219a1e47e1426aa05667f02
Edson733 <20213tn034@utez.edu.mx>
2024-02-22 02:11:12 -0600

    Terminado

--- matriz.js
+++ matriz.js
@@ -1,63 +1,183 @@
-let intervalo;
-let matrix = [];
-
-function crearMatriz(filas, columnas) {
-    matrix = [];
-    const tabla = document.getElementById('matriz');
-    tabla.innerHTML = '';
-
-    for (let i = 0; i < filas; i++) {
-        const fila = [];
-        const tr = document.createElement('tr');
+let matriz = []
+let iniciado = false
         
-        for (let j = 0; j < columnas; j++) {
-            const td = document.createElement('td');
-            fila.push(false);
-            tr.appendChild(td);
+function generarCoordenadasAleatorios(cuantosRandoms, limX, limY){
+    let coordenadasAleatorias = [];
+    for (let i = 0; i < cuantosRandoms; i++) {
+        let coordenadaX = Math.floor(Math.random() * limX + 1);
+        let coordenadaY = Math.floor(Math.random() * limY + 1);
+        coordenadasAleatorias.push({ x: coordenadaX - 1, y: coordenadaY - 1});
+    }
+    return coordenadasAleatorias;
+}
+
+function pintarTabla(matriz, x, y, tiempo, cuadros){
+    const contenedor_matriz = document.getElementById("matriz");
+    const numerosRandoms = generarCoordenadasAleatorios(cuadros, x, y)
+    let tabla = '<table>'
+
+    //verificar que los cuadros a pintar no esten pintados
+    let validorCambio = true
+    for(let i = 0; i < numerosRandoms.length; i++){
+        validorCambio = validorCambio && matriz[numerosRandoms[i].x][numerosRandoms[i].y] == 0
+    }
+
+    //si es valido cambiar a 1 las coordenadas
+    if(validorCambio){
+        for(let i = 0; i < numerosRandoms.length; i++){
+            matriz[numerosRandoms[i].x][numerosRandoms[i].y] = 1  
         }
-        matrix.push(fila);
-        tabla.appendChild(tr);
-    }
-}
-
-function iniciar() {
-    const filas = parseInt(document.getElementById('filas').value);
-    const columnas = parseInt(document.getElementById('columnas').value);
-    const tiempo = parseInt(document.getElementById('tiempo').value * 1000);
-    const cantidad = parseInt(document.getElementById('cantidad').value);
-
-    if (isNaN(filas) || isNaN(columnas) || isNaN(tiempo) || isNaN(cantidad) || filas < 1 || columnas < 1 || tiempo < 1 || cantidad < 1) {
-        alert('Por favor, complete todos los campos correctamente.');
-        return;
     }
 
-    crearMatriz(filas, columnas);
-    intervalo = setInterval(() => {
-        pintarCuadros(cantidad);
-    }, tiempo);
+    //mostrar tabla. las coordenadas que esten en 1 se pintaran de rojo
+    for (let i = 0; i < x; i++) {
+        tabla += '<tr>'
+        for (let j = 0; j < y; j++) {
+            if(matriz[i][j] == 1){
+                tabla += '<td style="background-color: blue;">' + matriz[i][j] + '</td>'
+            }else{
+                tabla += '<td>' + matriz[i][j] + '</td>'
+            }
+        }
+        tabla += '</tr>'
+    }
+    tabla += '</table>'
+    contenedor_matriz.innerHTML = tabla
+    setTimeout(() => {
+        pintarTabla(matriz, x, y, tiempo, cuadros)
+    },  tiempo * 1000);
 }
 
-function pintarCuadros(cantidad) {
-    for (let i = 0; i < cantidad; i++) {
-        const fil = Math.floor(Math.random() * matrix.length);
-        const col = Math.floor(Math.random() * matrix[0].length);
+function verificar(){
+    let filas = document.getElementById("filas").value
+    let columnas = document.getElementById("columnas").value
+    let tiempo = document.getElementById("tiempo").value
+    let cuadros = document.getElementById("cuadros").value
 
-        if (matrix[fil][col]) {
-            detener();
-            alert('El programa se ha detenido, alguno de los cuadros esta ocupado.');
-            return;            
+    let errores = new Map();
+
+    if(!iniciado){
+        iniciado = true
+        //validar los campos
+        //validar coordenada x individualmente
+        if(filas){
+            try {
+                filas = Number(filas)
+                if(Number.isInteger(filas)){
+                    if(filas > 0 && filas < 101){
+                        errores.set("filas", false)
+                    }else{
+                        alert("El campo de las Filas debe ser entre 1 y 100")
+                        errores.set("filas", true)
+                    }
+                }else{
+                    alert("El campo de las Filas debe ser un numero entero")
+                    errores.set("filas", true)
+                }
+            } catch (error) {
+                alert("El campo de las Filas debe ser un numero")
+                errores.set("filas", true)
+            }
+        }else{
+            alert("El campo de las Filas no puede estar vacio o contener caracteres especiales")
+            errores.set("filas", true)
         }
 
-        matrix[fil][col] = true;
-        document.getElementsByTagName('td')[row * matrix[0].length + col].style.backgroundColor = 'blue';
+        //validar coordenada y individualmente
+        if(columnas){
+            try {
+                columnas = Number(columnas)
+                if(Number.isInteger(columnas)){
+                    if(columnas > 0 && columnas <= 100){
+                        errores.set("columnas", false)
+                    }else{
+                        alert("El campo de las Columnas debe ser entre 1 y 100")
+                        errores.set("columnas", true)
+                    }
+                }else{
+                    alert("El campo de las Columnas debe ser un numero entero")
+                    errores.set("columnas", true)
+                }
+            } catch (error) {
+                alert("El campo de las Columnas debe ser un numero")
+                errores.set("columnas", true)
+            }
+        }else{
+            alert("El campo de las Columnas no puede estar vacio o contener caracteres especiales")
+            errores.set("columnas", true)
+        }
+
+        //validar tiempo individualmente
+        if(tiempo){
+            try {
+                tiempo = Number(tiempo)
+                if(Number.isInteger(tiempo)){
+                    if(tiempo > 0){
+                        errores.set("tiempo", false)
+                    }else{
+                        alert("El campo del tiempo debe ser mayor a 0")
+                        errores.set("tiempo", true)
+                    }
+                }else{
+                    alert("El campo del tiempo debe ser un numero entero")
+                    errores.set("tiempo", true)
+                }
+            } catch (error) {
+                alert("El campo del tiempo debe ser un numero")
+                errores.set("tiempo", true)
+            }
+        }else{
+            alert("El campo del tiempo no puede estar vacio o contener caracteres especiales")
+            errores.set("tiempo", true)
+        }
+
+        //validar cuadros individualmente
+        if(cuadros){
+            try {
+                cuadros = Number(cuadros)
+                if(Number.isInteger(cuadros)){
+                    if(cuadros > 0){
+                        errores.set("cuadros", false)
+                    }else{
+                        alert("El campo de los cuadros debe ser mayor a 0")
+                        errores.set("cuadros", true)
+                    }
+                }else{
+                    alert("El campo de los cuadros debe ser un numero entero")
+                    errores.set("cuadros", true)
+                }
+            } catch (error) {
+                alert("El campo de los cuadros debe ser un numero")
+                errores.set("cuadros", true)
+            }
+        }else{
+            alert("El campo de los cuadros no puede estar vacio o contener caracteres especiales")
+            errores.set("cuadros", true)
+        }
+
+        //validar otros posibles errores
+        if(filas * columnas <= cuadros){
+            alert("El numero de cuadros no puede ser mayor o igual a la cantidad de cuadros que tiene la matriz")
+            errores.set("cuadros", true)
+        }
+
+        if(filas * columnas % cuadros != 0){
+            alert("El numero de cuadros debe ser multiplo de la cantidad de cuadros que tiene la matriz")
+            errores.set("cuadros", true)
+        }
+
+        //validar errores
+        if(!errores.get("filas") && !errores.get("columnas") && !errores.get("tiempo") && !errores.get("cuadros")){
+            //crear matriz
+            for (let i = 0; i < filas; i++) {
+                matriz[i] = [];
+                for (let j = 0; j < columnas; j++) {
+                    matriz[i][j] = 0;
+                }
+            }
+            pintarTabla(matriz, filas, columnas, tiempo, cuadros)  
+        }
+    }else{
+        alert("Se ha pausado la ejecucion")
     }
-}
-
-function detener() {
-    clearInterval(intervalo);
-    limpiar();
-}
-
-function limpiar() {
-    location.reload();
 }
